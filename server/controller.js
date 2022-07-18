343008db98a25175b1c40190f3b081dc1fa3e62d
mohamed mohamed <mohamedmohamed@mohameds-MacBook-Air.local>
2022-07-18 08:46:50 -0500

    Assessment.4

---
+++ server/controller.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["A faithful friend is a strong defense.","A lifetime friend shall soon be made.","A lifetime of happiness lies ahead of you.","A pleasant surprise is waiting for you.","All your hard work will soon pay off."];
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+}
